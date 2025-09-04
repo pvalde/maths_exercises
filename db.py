d46bbb027bc88979c04405fd71c44a922538991b
Pedro Valdebenito <pedrovaldebto@gmail.com>
2025-09-04 12:50:38 -0400

    add first iteration of db schema

--- db.py
+++ db.py
@@ -5,24 +5,28 @@ import platformFuns
 def check_or_create_user_db() -> None:
     """
     Connects to user database. If there is no database, creates one following
-    a given squema.
+    a given schema.
     """
     try:
         with sqlite3.connect(platformFuns.get_user_db_path()) as conn:
             print(
-                f"Opened SQLite database with version {sqlite3.sqlite_version} successfully."
+                f"Opened SQLite3 database with version {sqlite3.sqlite_version} successfully."
             )
             cur = conn.cursor()
     except sqlite3.OperationalError as e:
         raise Exception("Failed to open database:", e)
 
-    # TODO: move definiton of program's squema to another file (a 'Constans' module)
+    # TODO: move definition of program's schema to another file (a 'Constants' module)
     cur.execute(
-        """CREATE TABLE IF NOT EXISTS test(
-                id INT PRIMARY KEY,
-                a TEXT,
-                b TEXT,
-                c TEXT);
+        """CREATE TABLE IF NOT EXISTS exercises(
+                exercise_id                 INTEGER PRIMARY KEY,
+                exercise_topic              TEXT,
+                exercise_review_count       INTEGER,
+                exercise_last_review_date   TEXT,
+                exercise_feedback           INTEGER,
+                exercise_src                TEXT,
+                exercise_TEXT,
+                ); 
     """
     )
 
--- main.py
+++ main.py
@@ -1,5 +1,3 @@
 import db
-import platformFuns
 
 db.check_or_create_user_db()
-print(platformFuns.get_list_of_users())
